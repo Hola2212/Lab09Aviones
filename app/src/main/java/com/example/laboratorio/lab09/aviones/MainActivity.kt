f04684bcde275ad50a1188f31ad6f35770a1153d
Hola2212 <juanpaflores06@gmail.com>
2026-09-06 18:27:22 -0600

    Corrected inicialization code.

--- app/src/main/java/com/example/laboratorio/lab09/aviones/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio/lab09/aviones/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.laboratorio.lab09.aviones.ui.navigation.StoreNavigation
 import com.example.laboratorio.lab09.aviones.ui.theme.Lab09AvionesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab09AvionesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                StoreNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab09AvionesTheme {
-        Greeting("Android")
     }
 }
